c0b8c3eecb04d73ae6090f1be9f0519a4217ddb5
Hue Vang <huevang100@gmail.com>
2016-12-19 02:17:05 -0600

    Initial Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$('.button').on('click', function() {
+		$('p').slideToggle();
+		$('li').slideToggle();
+		$('h3').slideToggle();
+		$('h4').slideToggle();
+
+	});
+
+});
